da21ecd5d254e5afad1f3034649c0e87e1fd2a0a
einghon-phoo <einghon.phoo@thitsaworks.com>
2024-07-02 23:15:52 +0630

    add dataframe summary codes

--- case study/case_study_analysis_in_R/ride_share_data_analysis.R
+++ case study/case_study_analysis_in_R/ride_share_data_analysis.R
@@ -4,7 +4,7 @@ library(janitor)
 library(ggplot2)
 library(dplyr)
 #importind data from csv
-ride_data_df <- read_csv("D:/googl_data_course/R_basic_exercises/case study/datasets/202006-divvy-tripdata/202004-divvy-tripdata.csv")
+ride_data_df <- read_csv("D:/google_data_course/R_basic_exercises/case study/datasets/202006-divvy-tripdata/202004-divvy-tripdata.csv")
 
 #exploring data structure
 head(ride_data_df)
@@ -24,6 +24,20 @@ group_counts <- ride_data_df %>%
   summarize(count = n())
 print(group_counts)
 
+#adding time taken column
+ride_data_df<- ride_data_df %>% 
+  mutate(total_time_taken = difftime(ended_at, started_at, units = "mins")) %>% 
+  arrange(total_time_taken)
+
+#checking total tile taken 
+ride_data_df <-  ride_data_df %>% 
+  filter(total_time_taken > 0)
+ride_data_df
 
 
-
+#Pipe funtion control + shift + m for pipe operator [filter, group by and then summarized]
+ride_data_df_summary <- ride_data_df %>%  
+  group_by(member_casual) %>% 
+  summarize(ride_count = n(),mean_time_taken = mean(total_time_taken, na.rm = T), total_time_taken = sum(total_time_taken, na.rm = T), max_time_taken = max(total_time_taken, na.rm = T),min_time_taken = min(total_time_taken, na.rm = T) )
+#na.rm = TRUE: This tells R to remove (ignore) any missing values (NAs) in the vector before calculating the mean
+print(ride_data_df_summary)
